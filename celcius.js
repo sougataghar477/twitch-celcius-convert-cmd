9b813b73d47fd62ec2e18efccfa7ab433874cd6c
sougataghar477 <50982886+sougataghar477@users.noreply.github.com>
2021-06-30 11:53:44 +0530

    Update celcius.js

--- celcius.js
+++ celcius.js
@@ -1,3 +1,4 @@
-/*For Nightbot only,copy the code below*/
-
-!addcom !cel $(eval (5*($(1)-32)/9).toFixed(2)) 
+/*Usage-> !cel Fahreneit*/
+/*Example typing "!cel 212" will show 100℃"
+/*For Nightbot only,simply copy the code below*/
+!addcom !cel $(eval (5*($(1)-32)/9).toFixed(2)+"℃") 
